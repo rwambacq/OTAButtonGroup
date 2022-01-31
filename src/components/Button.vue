c2e08a9f342e1ee74a62380f5f1855d091e432fc
rwambacq <ruben.wambacq@gmail.com>
2022-01-31 18:42:49 +0100

    create button group

---
+++ src/components/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{
+    filled,
+    empty: !filled,
+    btn: true
+  }">
+    {{filled ? "" : "+"}}
+  </div>
+</template>
+
+<script>
+// A button that can either be styled as a solid button or an empty 'add' button
+export default {
+  name: 'Button',
+  props: {
+    filled: {  // indicates whether the button is a filled button or an empty button, used to dynamically set styling
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .btn {
+    width: 36px;
+    height: 36px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  .filled {
+    background: #013A63;
+  }
+
+  .empty {
+    background: #EDFAFF;
+    border: 1px dashed #013A63;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.75rem;
+    font-weight: 600;
+  }
+</style>
---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="btn-group">
+    <Button v-for="index in buttons" :key="index"/>
+    <Button  @mousedown.native="increaseButtons()" :filled='false'/>
+  </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+// A group of buttons, aligned horizontally
+// the last button is an add button that can add other buttons
+export default {
+  name: 'ButtonGroup',
+  components: {
+    Button
+  },
+  data: function () {
+    return {
+      buttons: 0
+    }
+  },
+  methods: {
+    increaseButtons() {
+      this.buttons++;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn-group {
+  display: flex;
+  flex-flow: row wrap;
+  gap: 8px;
+  width: 100%;
+}
+</style>
